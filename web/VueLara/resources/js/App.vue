e22d7178f082ad1f337593e1f385c274f86f4b3f
tokatu4561 <ryobsk4561@gmail.com>
2021-11-20 14:38:44 -0800

    create auth page vue

--- web/VueLara/resources/js/App.vue
+++ web/VueLara/resources/js/App.vue
@@ -7,3 +7,15 @@
     </main>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
