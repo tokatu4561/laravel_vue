c68c08fe7e868cc29e6e96d4b9b235d0dbb3454b
tokatu4561 <ryobsk4561@gmail.com>
2021-11-20 22:15:01 -0800

    認証機能作成　ログイン済　リダイレクト

--- web/VueLara/routes/api.php
+++ web/VueLara/routes/api.php
@@ -22,3 +22,5 @@ Route::post('/register', 'Auth\RegisterController@register')->name('register');
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/user', function(){ return Auth::user(); });
